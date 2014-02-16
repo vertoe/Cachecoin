8344efb15519f6a5a2defd9a695c7ec9f497f1be
Michael Kitange <kalgecin@gmail.com>
2014-02-16 21:06:55 +0400

    filter out stale stake

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -10,7 +10,7 @@ bool TransactionRecord::showTransaction(const CWalletTx &wtx)
     if (wtx.IsCoinBase() || wtx.IsCoinStake())
     {
         // Ensures we show generated coins / mined transactions at depth 1
-        if (!wtx.IsInMainChain())
+        if (wtx.GetDepthInMainChain() < 2)
         {
             return false;
         }
